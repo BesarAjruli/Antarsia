d940b60e60719855b871022c3ed484c8e4c4018f
BesarAjruli <aquila1912@gmail.com>
2026-01-03 17:23:50 +0100

    Made the plus button open add dialog

--- client/src/Components/Add.jsx
+++ client/src/Components/Add.jsx
@@ -2,10 +2,10 @@ import Icon from "@mdi/react";
 import { mdiPlus, mdiPrinter } from "@mdi/js";
 import "./Add.css";
 
-function Add() {
+function Add({addDialog}) {
   return (
     <>
-      <div className="add">
+      <div className="add" onClick={() => addDialog.current.showModal()}>
         <div className="icon-wrapper">
           <Icon className="icon" path={mdiPlus} size={1.6} />
         </div>

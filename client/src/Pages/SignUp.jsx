9da5012c5853f57a423d0a49997f973d9cbf99ae
BesarAjruli <aquila1912@gmail.com>
2026-01-07 15:10:05 +0100

    Removed confirm pssw field, added name field

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -7,6 +7,12 @@ function SignUp() {
     <div className='login'>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLu7NUMFCS7GKcutI4sZPEA1sEpipcLifGng&s" alt="logo e antarsise" />
         <form method='post'>
+
+            <div>
+                <label htmlFor="name">Name:</label>
+                <br />
+                <input type="text" id='name' name='name' placeholder='Mosques name'/>
+            </div>
             <div>
                 <label htmlFor="email">Email:</label>
                 <br />
@@ -16,11 +22,6 @@ function SignUp() {
                 <label htmlFor="password">Password:</label>
                 <br />
                 <input type="password" id='password' name='passowrd' placeholder='********'/>
-            </div>
-            <div>
-                <label htmlFor="cnfPassword">Confirm Password:</label>
-                <br />
-                <input type="password" id='cnfPassword' name='cnfPassword' placeholder='********'/>
             </div>
             <button type='submit'>Submit</button>
         </form>

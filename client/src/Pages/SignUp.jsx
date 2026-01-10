0d240a732714b34f9c844860d77fee56d33c84a9
BesarAjruli <aquila1912@gmail.com>
2026-01-10 16:22:55 +0100

    Linked with backend, added functions for submiting of the  form

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,33 +1,96 @@
-import './Login.css'
+import { useState } from "react";
+import "./Login.css";
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const res = await fetch("http://localhost:8095/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        alert(data.message);
+        return;
+      }
+
+      alert("Registration successful");
+      console.log("User:", data);
+    } catch (err) {
+      console.error("Signup error:", err);
+      alert("Server error");
+    }
+  };
 
   return (
     <>
-    <div className='login'>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLu7NUMFCS7GKcutI4sZPEA1sEpipcLifGng&s" alt="logo e antarsise" />
-        <form method='post'>
-
-            <div>
-                <label htmlFor="name">Name:</label>
-                <br />
-                <input type="text" id='name' name='name' placeholder='Mosques name'/>
-            </div>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <br />
-                <input type="email" id="email" name='email' placeholder='example@gmail.com'/>
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <br />
-                <input type="password" id='password' name='passowrd' placeholder='********'/>
-            </div>
-            <button type='submit'>Submit</button>
+      <div className="login">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLu7NUMFCS7GKcutI4sZPEA1sEpipcLifGng&s"
+          alt="logo e antarsise"
+        />
+        <form method="post" onSubmit={submit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <br />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Mosques name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="example@gmail.com"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <br />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="********"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Submit</button>
         </form>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;

577df773ac1034c42df010a66e437fe1ac3742e2
BesarAjruli <aquila1912@gmail.com>
2026-01-10 16:57:59 +0100

    Formatted code, removed local storage cause of errors

--- backend-antarsia/src/controllers/auth.controller.js
+++ backend-antarsia/src/controllers/auth.controller.js
@@ -3,70 +3,69 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-        if (!name || !email || !password) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
-        const hashed = await bcrypt.hash(password, 10);
-
-        const user = await pool.query(
-            "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING id, name, email",
-            [name, email, hashed]
-        );
-
-        res.status(201).json(user.rows[0]);
-    } catch (err) {
-        console.error("Register error:", err);
-        res.status(500).json({ message: "Registration error", error: err.message });
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await pool.query(
+      "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING id, name, email",
+      [name, email, hashed]
+    );
+
+    res.status(201).json(user.rows[0]);
+  } catch (err) {
+    console.error("Register error:", err);
+    res.status(500).json({ message: "Registration error", error: err.message });
+  }
 };
 
 exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-            return res.status(400).json({ message: "Email and password are required" });
-        }
-
-        const result = await pool.query(
-            "SELECT * FROM users WHERE email = $1",
-            [email]
-        );
-
-        if (result.rows.length === 0) {
-            return res.status(401).json({ message: "Incorrect email or password"});
-        }
-
-        const user = result.rows[0];
-
-        const valid = await bcrypt.compare(password, user.password);
-
-        if (!valid) {
-            return res.status(401).json({ message: "Incorrect email or password" });
-        }
-
-        
-        if (!process.env.JWT_SECRET) {
-            throw new Error("JWT_SECRET mungon në .env");
-        }
-
-        const token = jwt.sign(
-            { id: user.id, name: user.name },
-            process.env.JWT_SECRET,
-            { expiresIn: "1d" }
-        );
-        localStorage.setItem("user", user)
-
-        res.json({
-            token,
-            user: { id: user.id, name: user.name, email: user.email }
-        });
-    } catch (err) {
-        console.error("Login error:", err);
-        res.status(500).json({ message: "failed login", error: err.message });
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
     }
+
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ message: "Incorrect email or password" });
+    }
+
+    const user = result.rows[0];
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      return res.status(401).json({ message: "Incorrect email or password" });
+    }
+
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET mungon në .env");
+    }
+
+    const token = jwt.sign(
+      { id: user.id, name: user.name },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      token,
+      user: { id: user.id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    console.error("Login error:", err);
+    res.status(500).json({ message: "failed login", error: err.message });
+  }
 };

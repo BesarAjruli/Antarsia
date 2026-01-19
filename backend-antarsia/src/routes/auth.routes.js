4d30cdd6434ae2151a2e2f41ec302f129caa09b6
BesarAjruli <aquila1912@gmail.com>
2026-01-19 15:33:42 +0100

    Added middelware to the routes of user

--- backend-antarsia/src/routes/auth.routes.js
+++ backend-antarsia/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const {register , login, getUser} = require("../controllers/auth.controller");
+const authMiddleware = require('../middlewares/auth.middleweare')
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/user", getUser)
+router.get("/user",authMiddleware, getUser)
 
 module.exports = router;

30de0154ae4d0ccb34164495e679b0c50edeaa47
BesarAjruli <aquila1912@gmail.com>
2026-02-11 14:24:03 +0100

    fixed log out button

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,5 +1,3 @@
-import { button } from "../Components/ui/button"; 
-import { LogOut } from "lucide-react";           
 import { useNavigate } from "react-router";
 import "./Header.css";
 
@@ -23,27 +21,16 @@ export function Header() {
   };
 
   return (
-    <div className="header flex items-center justify-between p-4 bg-gray-100">
+    <div className="header">
       {/* Logo + titulli */}
-      <div className="flex items-center gap-4">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLu7NUMFCS7GKcutI4sZPEA1sEpipcLifGng&s"
           alt="logo e antarsise"
-          className="h-12 w-12 rounded"
         />
-        <h1 className="text-xl font-bold">Antarsia e fese islame</h1>
-      </div>
+        <h1>Antarsia e fese islame</h1>
 
       {/* Butoni Logout */}
-      <button
-        variant="destructive"  
-        size="md"
-        onClick={logout}
-        style={{ backgroundColor: "green", color: "white", display: "flex", alignItems: "center", gap: "5px" }}
-      >
-        <LogOut className="h-5 w-5" />
-        Log Out
-      </button>
+      <button className="button">Log Out</button>
     </div>
   );
 }

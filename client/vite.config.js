7b9a3e9b36a15abc820627f75066a627d74745d5
BesarAjruli <aquila1912@gmail.com>
2026-02-11 14:36:07 +0100

    Fixed vite configs server errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+const backendUrl = process.env.VITE_BACKEND_URL;
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+  server: {
+    host: true,
+    proxy: {
+      "/api": backendUrl,
     },
   },
 });

6e106e248e581604ff8ee7b46ba0bdad27e7ba35
BesarAjruli <aquila1912@gmail.com>
2026-02-17 19:08:46 +0100

    Added loader

---
+++ client/src/Components/Loader.jsx
@@ -0,0 +1,20 @@
+import './Loader.css'
+
+function Loader() {
+  return (
+    <div class="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Loader;
